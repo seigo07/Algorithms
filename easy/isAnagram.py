57c57f2587bdcc75968b15412e898a2707cae3a8
seigo07 <seven_8s@icloud.com>
2023-10-07 00:50:06 +0100

    fix isAnagram

--- easy/isAnagram.py
+++ easy/isAnagram.py
@@ -1,8 +1,11 @@
+# 与えられた2つの文字列 s と t がアナグラムであるかどうかを判定する
+# アナグラムとは、異なる単語やフレーズの文字を並び替えて、
+# 元の文字をすべて正確に一度ずつ使用して新しい単語やフレーズを作成すること
+
+
 def isAnagram(s, t):
-    if len(s) != len(t):
-        return False
-    else:
-        return sorted(s) == sorted(t)
+    # 文字列をソートして比較することで、アナグラムかどうかを判定します
+    return sorted(s) == sorted(t)
 
 
 s = "anagram"
