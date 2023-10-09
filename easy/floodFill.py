aa40e66f294edd5b155094bc419e3ed7eb9fbc80
seigo07 <seven_8s@icloud.com>
2023-10-09 20:09:56 +0100

    fix floodFill

--- easy/floodFill.py
+++ easy/floodFill.py
@@ -1,30 +1,56 @@
+# DFSを使用して、指定された座標から始まる画像内の連結した領域を塗りつぶす
+
+
+# sr, sc: 開始位置
 def floodFill(image, sr, sc, newColor):
+
+    """
+    :param sr: 塗りつぶしを開始する座標の行インデックス。
+    :param sc: 塗りつぶしを開始する座標の列インデックス。
+    :param newColor: 新しい色の整数値。指定された座標から始まる領域内のすべてのピクセルがこの色に変更される。
+    """
+
+    # 初期位置の色（塗りつぶしを開始する色）を取得
     startColor = image[sr][sc]
+
+    # すでに指定された色に塗りつぶされている場合は何もしない
     if startColor == newColor:
         return image
-    directions = [(0, -1), (0, 1), (-1, 0), (1, 0)]
-    stack = [(sr, sc)]
 
-    while stack:
-        r, c = stack.pop()
-        if image[r][c] == startColor:
+    # DFSは再帰的なアルゴリズムであり、木やグラフ構造など、非線形なデータの探索に適している
+    # 指定された座標 (r, c) から連結した領域（下、上、右、左）を再帰的に探索し塗りつぶす
+    def dfs(r, c):
+
+        # (r, c)がindexを超えず、
+        # image[r][c]の色が startColor である場合にのみ処理を実行
+        if 0 <= r < len(image) and 0 <= c < len(image[0]) and image[r][c] == startColor:
+
+            # 現在のピクセルを newColor に変更
             image[r][c] = newColor
-            for dr, dc in directions:
-                nr, nc = r + dr, c + dc
-                if 0 <= nr < len(image) and 0 <= nc < len(image[0]) and image[nr][nc] == startColor:
-                    stack.append((nr, nc))
+
+            # 4方向（下、上、右、左）に対して再帰的に dfs を呼び出す
+            # 同じ色で連結した領域がすべて新しい色に変更される
+            dfs(r + 1, c) # 下
+            dfs(r - 1, c) # 上
+            dfs(r, c + 1) # 右
+            dfs(r, c - 1) # 左
+
+    # 深さ優先探索を開始
+    dfs(sr, sc)
 
     return image
 
 
-# 初期の画像を定義します。
-image = [[1, 1, 1], [1, 1, 0], [1, 0, 1]]
+# サンプル画像
+image = [
+    [1, 1, 1],
+    [1, 1, 0],
+    [1, 0, 1]
+]
 
-# 開始ピクセルは (1, 1) で、新しい色は 2 にします。
-sr, sc, newColor = 1, 1, 2
+# 画像を塗りつぶし
+newImage = floodFill(image, 1, 1, 2)
 
-# floodFill 関数を呼び出します。
-newImage = floodFill(image, sr, sc, newColor)
-
-# 新しい画像を出力します。
-print(newImage)
+# 結果の表示
+for row in newImage:
+    print(row)
