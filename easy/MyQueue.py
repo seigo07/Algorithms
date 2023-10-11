131d8a263294b1f81246cc8388c586f61f7f38e4
seigo07 <seven_8s@icloud.com>
2023-10-11 22:40:41 +0100

    fix MyQueue

--- easy/MyQueue.py
+++ easy/MyQueue.py
@@ -1,23 +1,36 @@
+# 2つのスタックを使用して、FIFO（First-In-First-Out）キューの
+# すべての機能（push、peek、pop、empty）を実装する
+
+
 class MyQueue:
     def __init__(self):
-        self.s1 = []
-        self.s2 = []
+        self.s1 = []  # 要素を追加するスタック
+        self.s2 = []  # 要素を取り出すための補助スタック
 
-    def push(self, x) -> None:
-        self.s1.append(x)
+    # s1に要素を追加するだけ
+    def push(self, value):
+        self.s1.append(value)
 
-    def pop(self) -> int:
+    # 最も古い要素を取り出す
+    def pop(self):
+        # stack2が空である場合、stack1から要素をすべて取り出して逆順にstack2に移動する
+        # これにより、最も古い要素がstack2の先頭にくるようになる
         self.peek()
+        # stack2から要素を取り出し、FIFOの動作を実現
         return self.s2.pop()
 
-    def peek(self) -> int:
+    # キューの最後の要素（キューの先頭要素）を取得（取り出しは行わない）
+    def peek(self):
+        # popと同じ処理（取り出しは行わない）
         if not self.s2:
             while self.s1:
                 self.s2.append(self.s1.pop())
+        # スタックの最後の要素を取得する（キューの先頭にある要素）
         return self.s2[-1]
 
-    def empty(self) -> int:
+    def empty(self):
         return not self.s1 and not self.s2
+
 
 queue = MyQueue()
 
