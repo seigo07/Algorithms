7d646a425b539985fbbbb95c590ac8e1f5d7e39e
seigo07 <seven_8s@icloud.com>
2023-10-13 20:52:42 +0100

    fix is_palindrome

--- easy/isPalindrome.py
+++ easy/isPalindrome.py
@@ -1,18 +1,19 @@
 # 与えられた文字列が回文（文字列が前から読んでも後ろから読んでも同じか）かどうかを判定する
 
 
-def isPalindrome(s):
+def is_palindrome(s):
 
-    # 文字列を小文字に変換し、文字と数字以外の文字を無視
-    # ''.joinにより、文字列間に何も挿入され図、リスト内の各文字がそのまま連結される
+    # 文字列を小文字に変換。
+    # 文字と数字以外の文字を無視
+    # ''.joinにより、文字列間に何も挿入されず、リスト内の各文字がそのまま連結される
     s = ''.join(e.lower() for e in s if e.isalnum())
 
     # 文字列が前から読んでも後ろから読んでも同じかどうかを判定
-    # s[::-1]はsの逆文を表す
+    # s[::-1]は s の逆文を表す
     return s == s[::-1]
 
 
-print(isPalindrome("A man, a plan, a canal: Panama"))  # Returns True
-print(isPalindrome("race car"))  # Returns True
-print(isPalindrome("Hello, world"))  # Returns False
+print(is_palindrome("A man, a plan, a canal: Panama"))  # Returns True
+print(is_palindrome("race car"))  # Returns True
+print(is_palindrome("Hello, world"))  # Returns False
 
