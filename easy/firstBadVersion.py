1daf569ea93a3a6a4f58943287e34c7dee56d1b5
seigo07 <seven_8s@icloud.com>
2023-10-13 08:38:43 +0100

    add first_bad_version

---
+++ easy/firstBadVersion.py
@@ -0,0 +1,40 @@
+# 与えられた一連のバージョンの中から、最初のバッドバージョンを特定する
+# 最小限のAPI呼び出しを行うため、二分探索を使用する
+
+
+def first_bad_version(n):
+    left, right = 1, n
+
+    while left < right:
+
+        # 中間値を計算
+        mid = left + (right - left) // 2
+        print(mid)
+
+        if is_bad_version(mid):
+            # 中間バージョンがバッドバージョンである場合、中間値から左半分に探索範囲を絞る
+            # e.g. 1 ~ 5 -> 1 ~ right(3) に絞る
+            right = mid
+        else:
+            # 中間バージョンが正常な場合、中間値から右半分に探索範囲を絞る
+            # e.g. 1 ~ 5 -> left(3) ~ 5 に絞る
+            left = mid + 1
+
+    # leftとrightの値が一致すると、ループを抜ける
+    # それが最初のバッドバージョン
+    return left
+
+
+# サンプルの isBadVersion 関数
+def is_bad_version(version):
+    # 例として、バッドバージョンが4番目の場合を想定
+    return version >= 4
+
+
+# バージョン数
+n = 5
+
+# 最初のバッドバージョンを見つける
+first_bad = first_bad_version(n)
+
+print("最初のバッドバージョンは:", first_bad)
--- easy/mergeTwoLists.py
+++ easy/mergeTwoLists.py
@@ -7,26 +7,35 @@ class ListNode:
         self.next = next
 
 
-def mergeTwoLists(list1, list2):
+def merge_two_lists(list1, list2):
 
-    # 新しいリンクリストの先頭を表すダミーノード（マージ操作の簡略化のために使用）
+    # マージ操作の簡略化のためのダミーノード
     dummy = ListNode()
-    current = dummy  # 現在のノードをダミーノードに設定
+    # ポインタを渡す
+    current = dummy
 
     while list1 and list2:
-        # list1とlist2を比較し、小さい方の要素を新しいリンクリストに追加
+        # 小さい方の要素を新しいリンクリストに追加
         if list1.val < list2.val:
-            # current ポインタの次のノードを list1 の現在のノードに設定
-            # current ポインタが新しいリストで次に追加されるノードである list1 を指す
             current.next = list1
+            # 次のノードに進める
             list1 = list1.next
         else:
             # current ポインタの次のノードを list2 の現在のノードに設定
             # current ポインタが新しいリストで次に追加されるノードである list2 を指す
             current.next = list2
+            # 次のノードに進める
             list2 = list2.next
         # ポインタを次のノードに進める
+        # 操作中のリストが入る
+        # e.g.
+        # 1 -> 3 -> 4 ->
+        # 1 -> 2 -> 4 ->
+        # 2 -> 4 ->
+        # 3 -> 4 ->
+        # 4 ->
         current = current.next
+        print(current.val)
 
     # どちらかのリストが空になった場合、残りのリストを追加
     if list1:
@@ -41,7 +50,7 @@ list1 = ListNode(1, ListNode(2, ListNode(4)))
 list2 = ListNode(1, ListNode(3, ListNode(4)))
 
 # リストをマージして、新しいソート済みリンクリストを取得
-merged_list = mergeTwoLists(list1, list2)
+merged_list = merge_two_lists(list1, list2)
 
 # 結果を出力
 while merged_list:
