e23af892acae12115f7a367a0723f9951fba402c
Seigo Sugawara <seigo.sugawara@avanade.com>
2025-04-10 11:20:12 +0900

    fix comment

---
+++ easy/longest_palindrome.py
@@ -0,0 +1,24 @@
+
+# 与えられた文字列 s から作成可能な最長の回文の長さを求める
+
+from collections import Counter
+
+def longest_palindrome(s):
+    count = Counter(s)  # 文字ごとの出現回数をカウント
+    length = 0
+    odd_found = False
+    # 各文字とその出現回数に対して処理を行う
+    for char, freq in count.items():
+        length += (freq // 2) * 2   # 各文字の偶数分を回文の左右に使えるように加算
+        # 奇数個の文字があれば、1文字だけ中央に置ける
+        if freq % 2 == 1:
+            odd_found = True
+
+    # 中央に1文字だけ置ける場合 +1
+    if odd_found:
+        length += 1
+
+    return length
+
+print(longest_palindrome("abccccdd"))   # 出力: 7
+print(longest_palindrome("a"))          # 出力: 1
---
+++ easy/reverse_list.py
@@ -0,0 +1,83 @@
+
+'''
+
+与えられた単方向リンクリストを後ろから前に逆順にし、新しい先頭ノード（反転後のリストのhead）を返す
+
+ex.
+初期状態
+prev = None
+curr = 1 -> 2 -> 3 -> 4 -> 5
+
+1回目ループ
+next_node = 2
+curr.next = None
+prev = 1
+curr = 2
+
+2回目ループ
+next_node = 3
+curr.next = 1
+prev = 2
+curr = 3
+
+3回目ループ
+next_node = 4
+curr.next = 2
+prev = 3
+curr = 4
+
+4回目ループ
+next_node = 5
+curr.next = 3
+prev = 4
+curr = 5
+
+5回目ループ
+next_node = None
+curr.next = 4
+prev = 5
+curr = None（終了）
+
+最終状態
+prev = 5 -> 4 -> 3 -> 2 -> 1 -> None
+→ これが反転後のhead
+
+'''
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def reverse_list(head):
+    prev = None # 逆順で構築中の新しいリンク先
+    curr = head # 現在のノードをheadからスタート
+    # 現在のノードが存在する限りループ
+    while curr:
+        next_node = curr.next  # 次のノードを保持
+        curr.next = prev       # 現在のノードの次を前のノードに変更（反転）
+        prev = curr            # prevを一つ前に進める
+        curr = next_node       # 現在のノードを次のノードに進める
+    return prev  # 新しいhead
+
+
+def printList(head):
+    while head:
+        print(head.val, end=" -> ")
+        head = head.next
+    print("None")
+
+# リスト作成: 1 -> 2 -> 3 -> 4 -> 5 -> None
+node5 = ListNode(5)
+node4 = ListNode(4, node5)
+node3 = ListNode(3, node4)
+node2 = ListNode(2, node3)
+head = ListNode(1, node2)
+
+print("Before reverse:")
+printList(head)
+
+reversed_head = reverse_list(head)
+
+print("After reverse:")
+printList(reversed_head)
