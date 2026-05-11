ffab081f02b601397dec2b71d8f25fa52d164c56
seigo07 <seven_8s@icloud.com>
2026-05-11 20:04:57 +0900

    fix maxProfit

--- easy/maxProfit.py
+++ easy/maxProfit.py
@@ -1,21 +1,17 @@
-# 与えられた株価の配列から最大の利益を見つける
-# 与えられた配列 prices は、各日の株価を表している
-# この配列内で株を一度だけ買い、将来のある日にそれを売却して利益を最大化するのが目的
-
-
+# 一番安い日に買って、その後の一番高い日に売ると利益が最大になる
+# 時間計算量: O(n)
+# 空間計算量（メモリ）: O(1)
 def max_profit(prices):
-    # 最初のループで値を最小値として保持するため、無限大の値を設定
-    min_price = float("inf")
-    # 最初の最大利益は0
-    max_profit = 0
+    
+    min_price = float("inf")    # 最初のループで値を最小値として保持するため、無限大の値を設定
+    max_profit = 0    # 最初の最大利益は0
 
     for price in prices:
-        if price < min_price:
-            # より小さい価格が見つかったら最小価格を更新
-            min_price = price
-        else:
-            # より大きな利益が見つかったら最大利益を更新
-            max_profit = max(max_profit, price - min_price)
+        min_price = min(min_price, price)   # より小さい価格が見つかったら最安値を更新
+        profit = price - min_price   # 今日売った場合の利益
+        max_profit = max(max_profit, profit)    # より大きな利益が見つかったら最大利益を更新
+
+    return max_profit
 
 
 prices = [7, 1, 5, 3, 6, 4]
