09c7975d14cbc256a024a5f7c1bba074e866801b
seigo07 <seven_8s@icloud.com>
2026-05-21 22:14:59 +0900

    fix myqueue

--- easy/myqueue.py
+++ easy/myqueue.py
@@ -1,35 +1,33 @@
-# 2つのスタックを使用して、FIFO（First-In-First-Out）キューの
-# すべての機能（push、peek、pop、empty）を実装する
-
-
+# 2つのスタックを使用して、FIFO QUEUEのすべての機能（push、peek、pop、empty）を実装する
 class myqueue:
     def __init__(self):
-        self.s1 = []  # 要素を追加するスタック
-        self.s2 = []  # 要素を取り出すための補助スタック
+        self.in_stack = []  # push用
+        self.out_stack = [] # pop / peek 用
 
-    # s1に要素を追加するだけ
+    # 新しい要素は in_stack に積む
     def push(self, value):
-        self.s1.append(value)
+        self.in_stack.append(value)
 
-    # 最も古い要素を取り出す
+    # out_stack が空なら in_stack から移す
     def pop(self):
-        # stack2が空である場合、stack1から要素をすべて取り出して逆順にstack2に移動する
-        # これにより、最も古い要素がstack2の先頭にくるようになる
+        # out_stackが空の場合、in_stackから要素をすべて取り出して逆順にout_stackに移動する
+        # これにより、最も古い要素がout_stackの先頭にくるようになる
         self.peek()
-        # stack2から要素を取り出し、FIFOの動作を実現
-        return self.s2.pop()
+        # out_stackから要素を取り出し、FIFOの動作を実現
+        return self.out_stack.pop()
 
-    # キューの最後の要素（キューの先頭要素）を取得（取り出しは行わない）
+    # 先頭要素を見る（取り出しは行わない）
     def peek(self):
-        # popと同じ処理（取り出しは行わない）
-        if not self.s2:
-            while self.s1:
-                self.s2.append(self.s1.pop())
-        # スタックの最後の要素を取得する（キューの先頭にある要素）
-        return self.s2[-1]
+        # out_stack が空の時だけ移動して FIFO 順にする
+        if not self.out_stack:
+            while self.in_stack:
+                self.out_stack.append(self.in_stack.pop())
+        # out_stackの最後の要素を取得する（キューの先頭にある要素）
+        return self.out_stack[-1]
 
+    # 両方空なら Queue も空
     def empty(self):
-        return not self.s1 and not self.s2
+        return not self.in_stack and not self.out_stack
 
 
 queue = myqueue()
