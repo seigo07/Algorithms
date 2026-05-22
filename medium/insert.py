003d43862567cefe77c7e13b50a47d593b56582e
seigo07 <seven_8s@icloud.com>
2026-05-22 14:15:19 +0900

    fix insert

--- medium/insert.py
+++ medium/insert.py
@@ -1,43 +1,29 @@
-# You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi]
-# represent the start and the end of the ith interval and intervals is sorted in ascending order by starti.
-# You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+# ソート済み・重複なし区間リストに new_interval を挿入し、重なる区間をマージする
+# 時間空間計算量: O(n)
+def insert(intervals, new_interval):
+    result = []
+    start, end = new_interval
 
-# 重複しない間隔の配列が与えられます。
-# interval[i] = [starti, endi] は i 番目の間隔の開始と終了を表し、間隔は starti によって昇順に並べ替えられます。
-# また、別の間隔の開始と終了を表す間隔 newInterval = [start, end] も指定されます。
-# newInterval を間隔に挿入して、間隔が starti によって昇順にソートされ、間隔に重複する間隔が存在しないようにします (必要に応じて重複する間隔をマージします)。
-# 挿入後の間隔を返します。
+    for i, (s, e) in enumerate(intervals):
+        # new_interval が今の区間より前にある → ここで挿入確定
+        if end < s:
+            i -= 1
+            break
+        # new_interval が今の区間と重複 → new_interval を広げる
+        elif start <= e:
+            start = min(start, s)
+            end = max(end, e)
+        # new_interval が今の区間より後ろ → そのまま追加
+        else:
+            result.append([s, e])
 
-# このプログラムは、与えられた間隔のリストに新しい間隔を挿入し、必要に応じて重複する間隔をマージする機能を提供します。
-class Solution(object):
-    def insert(self, intervals, newInterval):
-        # 結果を格納するためのリスト
-        result = []
-        # 新しいインターバル
-        start, end = newInterval
+    return result + [[start, end]] + intervals[i+1:]
 
-        for i, (s, e) in enumerate(intervals):
-            # 新しいインターバルが現在のインターバルよりも後ろにある場合
-            if end < s:
-                i -= 1
-                break
-            # 新しいインターバルが現在のインターバルと重複している場合
-            elif start <= e:
-                start = min(start, s)
-                end = max(end, e)
-            else:
-                result.append([s, e])
-
-        return result + [[start, end]] + intervals[i+1:]
-
-
-# インスタンスの作成
-solution = Solution()
 
 intervals = [[1,3],[6,9]]
-newInterval = [2,5]
-print(solution.insert(intervals, newInterval))
+new_interval = [2,5]
+print(insert(intervals, new_interval))
 
 intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]]
-newInterval = [4,8]
-print(solution.insert(intervals, newInterval))
+new_interval = [4,8]
+print(insert(intervals, new_interval))
