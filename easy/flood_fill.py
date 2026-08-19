62f2f0a9d2fc86323a8ec56466a718ba43254266
seigo07 <seven_8s@icloud.com>
2026-08-19 09:44:22 +0900

    clean

--- easy/flood_fill.py
+++ easy/flood_fill.py
@@ -1,28 +1,28 @@
 # DFSを使用して、指定された座標 (r, c) から連結した領域（下、上、右、左）を再帰的に探索し塗りつぶす
 # 時間・空間計算量: O(mn) m = 行数, n = 列数 最悪全マス見る キューに最大で全マス近く入る可能性がある
-def flood_fill(image, sr, sc, new_color):
+def flood_fill(image, sr, sc, color):
 
     """
     :param sr: 塗りつぶしを開始する座標の行インデックス。
     :param sc: 塗りつぶしを開始する座標の列インデックス。
-    :param new_color: 新しい色の整数値。指定された座標から始まる領域内のすべてのピクセルがこの色に変更される。
+    :param color: 新しい色の整数値。指定された座標から始まる領域内のすべてのピクセルがこの色に変更される。
     """
 
     # 塗りつぶしを開始する位置の色を取得
     start_color = image[sr][sc]
 
     # すでに指定された色に塗りつぶされている場合は何もしない
-    if start_color == new_color:
+    if start_color == color:
         return image
 
     # 指定された座標 (r, c) から連結した領域（下、上、右、左）を再帰的に探索し塗りつぶす
     def dfs(r, c):
 
-        # (r, c)がindexを超えず、image[r][c]の色が start_color である場合にのみ処理を実行
+        # (r, c)がindex範囲を超えず、image[r][c]の色が start_color である場合にのみ処理を実行
         if 0 <= r < len(image) and 0 <= c < len(image[0]) and image[r][c] == start_color:
 
-            # 現在のピクセルを new_color に変更
-            image[r][c] = new_color
+            # 現在のピクセルを color に変更
+            image[r][c] = color
 
             # 4方向（下、上、右、左）に対して再帰的に dfs を呼び出す
             # 同じ色で連結した領域がすべて新しい色に変更される
