d76b507490728adadbbf25d9405c1dde5e6c87c8
seigo07 <seven_8s@icloud.com>
2026-09-02 12:52:50 +0900

    fix three_sum

--- medium/three_sum.py
+++ medium/three_sum.py
@@ -1,36 +1,38 @@
-# Given an integer array nums, return all the triplets
-# [nums[i], nums[j], nums[k]] such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+# 配列 nums から、a + b + c = 0 となる重複しない3つの数字の組み合わせをすべて探す
+# 時間計算量: O(n²) 各 i に対して l, r を最大n回動かす
+# 空間計算量: O(n) Pythonの sort() が最悪O(n)程度の補助領域を使用
 
-# 整数配列 nums を指定すると、i != j、i != k、j != k、および nums[i] + となるすべてのトリプレット [nums[i], nums[j], nums[k]] を返します。 nums[j] + nums[k] == 0。
-
-# このプログラムは、整数の配列 nums から、合計が0になる3つの数の組を探します。
+# ソート + 2ポインタ(left, right)
 def three_sum(nums):
-
-    def two_sum(nums, i, result):
-        seen = set()
-        j = i + 1
-        while j < len(nums):
-            complement = -nums[i] - nums[j]
-            if complement in seen:
-                result.append([nums[i], nums[j], complement])
-                while j + 1 < len(nums) and nums[j] == nums[j + 1]:  # 重複を避ける
-                    j += 1
-            seen.add(nums[j])
-            j += 1
-
-
     nums.sort()
-    result, visited = [], set()
+    ans = []
+    # 3つの要素を選ぶので、i は最後から3番目まで
+    for i in range(len(nums) - 2):
+        # iが同じ前の数と同じならスキップして重複を防ぐ 
+        # ex. nums = [-4, -1, -1, 0, 1, 2] で i = 2 のとき、nums[i] == nums[i - 1] # -1 == -1
+        if i > 0 and nums[i] == nums[i - 1]:
+            continue
 
-    for i in range(len(nums)):
-        if nums[i] > 0:  # 三つの数の合計が0になるため、最小の数が正の場合、以降の数を探索する必要はありません
-            break
-        if i == 0 or nums[i] != nums[i - 1]:  # 同じ数を二度探索しないためのチェック
-            two_sum(nums, i, result)
+        l = i + 1
+        r = len(nums) - 1
+        while l < r:
+            s = nums[i] + nums[l] + nums[r]
+            if s < 0:
+                l += 1
+            elif s > 0:
+                r -= 1
+            else:
+                ans.append([nums[i], nums[l], nums[r]])
+                l += 1
+                r -= 1
+                # l,r が同じ前の数と同じならスキップして重複を防ぐ 
+                while l < r and nums[l] == nums[l-1]:
+                    l += 1
+                while l < r and nums[r] == nums[r+1]:
+                    r -= 1
+    return ans
 
-    return result
 
-
-# 使用例:
+# 実行
 nums = [-1, 0, 1, 2, -1, -4]
 print(three_sum(nums))
